147916eb279c9ffca566b3249650a0d7202eb114
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2020-06-09 00:23:29 -0300

    arreglos al tp4 incorporando getter y setter

--- 58003.Ruggeri.Martin/tarea 4/person.py
+++ 58003.Ruggeri.Martin/tarea 4/person.py
@@ -1,17 +1,17 @@
 class Person():
-    def __init__(self, key="", nombre="", apellido="", edad=0):
+    def __init__(self, key=0, nombre="", apellido="", edad=0):
         self.key = key
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
 
     def __str__(self):
-        return "%s %s %s %d" % (self.key, self.nombre, self.apellido,
+        return "%d %s %s %d" % (self.key, self.nombre, self.apellido,
                                 self.edad)
 
-    def ingresar(self):
+    def ingresar(self, key):
         print("Ingresando Person")
-        self.key = input("Key: ")
+        self.key = key
         self.nombre = input("Ingrese nombre: ").upper()
         self.apellido = input("Ingrese apellido : ").upper()
         self.edad = int(input("Ingrese edad: "))
@@ -24,3 +24,35 @@ class Person():
             self.apellido = input("Ingrese apellido : ").upper()
         elif modificar == 3:
             self.edad = int(input("Ingrese edad: "))
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, value):
+        self._key = value
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, value):
+        self._nombre = value.upper()
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, value):
+        self._apellido = value.upper()
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, value):
+        self._edad = value
