0857640c627c096a5b437e7be1d556b34b3108dc
AgnidDrage <marianosancheztoledo@gmail.com>
2020-08-25 12:41:11 -0300

    Actualizacion

--- 59081.SanchezToledo.Mariano/04-trabajo/main.py
+++
@@ -1,8 +0,0 @@
-from person import Person
-from personService import PersonService
-from opciones import Opciones
-
-if __name__ == '__main__':
-    opciones = Opciones()
-    opciones.inicio()
-    
