f7b9b5388d35da4eb61933b427f61985caad863b
Oriel-Barroso <oriel_gold97@outlook.com>
2020-08-29 02:18:32 -0300

    TP7DOCK

--- 59163.Barroso.Oriel/clase.2020.04.21/test_punto.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from punto import Punto, PuntoMejorado
-from parameterized import parameterized
-class TestPunto(unittest.TestCase):
-    def test_str_punto(self, x, y, string):
-        punto = Punto(x, y)
-        self.assertEqual(punto.__str__(), string)
-if __name__ == "__main__":
-    unittest.main()
--- 59163.Barroso.Oriel/clase.2020.04.28/diagnostico.py
+++
@@ -1,5 +0,0 @@
-class CompuTools: 
-    def is_sorted(self, CompuTools):
-        if CompuTools == sorted(CompuTools):
-            return True
-                
