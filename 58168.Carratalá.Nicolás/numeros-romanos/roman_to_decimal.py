ac746bd518ea8e7b099235a7325ba05df4d8eb12
nicolascarratala <nicocarratala@gmail.com>
2020-10-28 12:56:08 -0300

    TrabajoPractico

--- 58168.Carratalá.Nicolás/numeros-romanos/roman_to_decimal.py
+++
@@ -1,6 +0,0 @@
-
-def roman_to_decimal(romanNum):
-    if romanNum == "I":
-        return 1
-    elif romanNum == "II":
-        return 2
--- 58168.Carratalá.Nicolás/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python test_roman_numbers.py
-
-python test_punto.py
-
-python test_sorting.py
